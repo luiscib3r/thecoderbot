4c6b356964b78de5cdb18171401c9d0f7c65aa58
Luis Antonio Correa Leyva <correaleyval@gmail.com>
2021-05-12 12:23:50 -0400

    Code queries

--- app/commands/start.py
+++ app/commands/start.py
@@ -1,21 +1,48 @@
-from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext, CommandHandler, ConversationHandler
 
 from app.models import TelegramUser
 from app.crud.telegram_user import crud_create_telegram_user
+from app.messages import start_message
 
-start_message = '''
-Hola soy TheCoderBot.
-Usa el comando /code para enviarme tu código y te responderé con una imagen de tu código resaltado.
-'''
+from app.queries.code import code_button, CODE, CODE_INPUT, CODE_CANCEL, code_handler, code_input_handler, code_cancel_handler
+
+INIT = "INIT"
 
 
 def start_handler(update: Update, context: CallbackContext):
     user = TelegramUser(**update.message.from_user.to_dict())
+    context.user_data['name'] = user.x_name
 
     crud_create_telegram_user(user)
 
-    update.message.reply_text(start_message)
+    update.message.reply_text(start_message.format(user.x_name), reply_markup=InlineKeyboardMarkup([
+        [code_button],
+        [
+            InlineKeyboardButton("👨‍💻 @luis_ciber", url="https://t.me/luis_ciber"),
+        ]
+    ]))
+
+    update.message.delete()
+
+    return INIT
 
 
-start_command = CommandHandler("start", start_handler)
+start_command = ConversationHandler(
+    entry_points=[
+        CommandHandler("start", start_handler),
+        code_handler,
+    ],
+    states={
+        INIT: [
+            code_handler,
+        ],
+        CODE_INPUT: [
+            code_input_handler,
+            code_cancel_handler,
+        ]
+    },
+    fallbacks=[
+        CommandHandler("start", start_handler),
+    ]
+)
